7639916c0ac3708ba06eea67ed2799e107e005aa
ShiLei <2640057431@qq.com>
2021-08-03 11:22:09 +0800

    添加注释并进行代码重构

--- app/src/main/java/person/shilei/musicplayer/util/LocalMusicUtils.kt
+++ app/src/main/java/person/shilei/musicplayer/util/LocalMusicUtils.kt
@@ -1,42 +1,23 @@
 package person.shilei.musicplayer.util
 
-
-import android.annotation.SuppressLint
-import android.content.ContentResolver
 import android.content.ContentUris
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Matrix
-import android.media.MediaMetadataRetriever
 import android.net.Uri
+import android.os.Build
 import android.provider.MediaStore
 import person.shilei.musicplayer.model.Song
 
-import java.io.FileDescriptor
-import java.io.FileNotFoundException
-import java.io.IOException
-import java.io.InputStream
-
-
 object LocalMusicUtils {
-    //定义一个集合，存放从本地读取到的内容
-    private lateinit var list: MutableList<Song>
-    private var name: String? = null
-    private var singer: String? = null
-    private var uri: Uri? = null
-    private var duration = 0
-    private var size: Long = 0
-    private var albumId: Long = 0
-    private var id: Long = 0
 
-    //获取专辑封面的Uri
-    private val albumArtUri: Uri = Uri.parse("content://media/external/audio/albumart")
-
-    private val albumArtString = "content://media/external/audio/albumart/"
-
+    /**
+     * 从MediaStore里获取本地音乐的信息，包括Uri路径等
+     *
+     * @param context
+     * @return 本地音乐列表
+     */
     fun getMusic(context: Context): MutableList<Song> {
-        list = mutableListOf()
+        val list = mutableListOf<Song>()
+        //从本地数据库中获取外部存储的音乐文件信息，并已添加日期倒序排列
         val cursor = context.contentResolver.query(
             MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
             null,
@@ -46,32 +27,34 @@ object LocalMusicUtils {
         )
         if (cursor != null) {
             while (cursor.moveToNext()) {
-
-                name =
+                var name =
                     cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME))
-                id = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID))
+                val id = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID))
 
-                uri = ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,id)
+                val uri = ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,id)
 
-                singer =
-                    cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST))
+                var singer = ""
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    singer =
+                        cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST))
+                }
 
-                duration =
-                    cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION))
-                size = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.SIZE))
-                albumId =
+                var duration = 0
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    duration =
+                        cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION))
+                }
+
+                val size = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.SIZE))
+                val albumId =
                     cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM_ID))
-                //list.add(song);
 
                 //把歌曲名字和歌手切割开,如果文件大于800KB，则为音乐文件
                 if (size > 1000 * 800) {
-                    if (name!!.contains(" - ")) {
-                        val str = name!!.split(" - ".toRegex()).toTypedArray()
-                        if (str.size == 2){
-                            singer = str[0].trim()
-                            name = str[1].trim()
-                        }
-                    }
+                    //将音乐文件名分割为歌曲名和歌手名
+                    val pair = splitNameSinger(name, singer)
+                    name = pair.first
+                    singer = pair.second
                     val song = Song(name,singer,size,duration,uri,albumId,id)
                     list.add(song)
                 }
@@ -82,7 +65,36 @@ object LocalMusicUtils {
         return list
     }
 
-    //    转换歌曲时间的格式
+    /**
+     * 例："周杰伦 - 七里香"
+     * 分离歌手和歌名
+     *
+     * @param name
+     * @param singer
+     * @return 返回分割后的歌手和歌名
+     */
+    fun splitNameSinger(
+        name: String?,
+        singer: String
+    ): Pair<String?, String> {
+        var name1 = name
+        var singer1 = singer
+        if (name1!!.contains(" - ")) {
+            val str = name1.split(" - ".toRegex()).toTypedArray()
+            if (str.size == 2) {
+                singer1 = str[0].trim()
+                name1 = str[1].trim()
+            }
+        }
+        return Pair(name1, singer1)
+    }
+
+    /**
+     * 转换歌曲时间的格式
+     *
+     * @param time 单位为毫秒
+     * @return 返回格式为‘x:xx’格式的时间字符串
+     */
     fun formatTime(time: Int): String {
         return if (time / 1000 % 60 < 10) {
             (time / 1000 / 60).toString() + ":0" + (time / 1000 % 60)
@@ -91,6 +103,13 @@ object LocalMusicUtils {
         }
     }
 
+    /**
+     * 获取歌曲的专辑图片
+     * 大部分mp3歌曲是没有专辑图片的，flac格式大概率会有
+     *
+     * @param album_id 歌曲的专辑id
+     * @return 专辑图片的Uri地址
+     */
     fun getArtQuick(album_id: Long): Uri {
         val artworkUri = Uri.parse("content://media/external/audio/albumart")
         return ContentUris.withAppendedId(artworkUri, album_id)
